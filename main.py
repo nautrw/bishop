8082508f08a31d32ffbfa68704367f531da103af
nautrw <nautrw@proton.me>
2026-08-26 18:27:38 -0400

    feat: actually implement algorithm

--- main.py
+++ main.py
@@ -1,9 +1,53 @@
-from hash import encode_text, bytes_to_pairs
+from ctypes.wintypes import CHAR
 from drawing import print_graph
+from hash import bytes_to_pairs, encode_text
 
 GRAPH_WIDTH = 17
 GRAPH_HEIGHT = 9
 
-initial_graph = [[0 for _ in range(GRAPH_WIDTH)] for _ in range(GRAPH_HEIGHT)]
+CHARACTERS = {
+    0: " ",
+    1: ".",
+    2: "o",
+    3: "+",
+    4: "=",
+    5: "*",
+    6: "B",
+    7: "0",
+    8: "X",
+    9: "@",
+    10: "%",
+    11: "&",
+    12: "#",
+    13: "/",
+    14: "^",
+    15: "S",
+    16: "E",
+}
 
 
+def run_algorithm(byte_pairs: list[str]) -> list[list[int]]:
+    graph = [[0 for _ in range(GRAPH_WIDTH)] for _ in range(GRAPH_HEIGHT)]
+    bishop_x = GRAPH_WIDTH // 2
+    bishop_y = GRAPH_HEIGHT // 2
+
+
+    for pair in byte_pairs:
+        bishop_x += 1 if pair[1] == "1" else -1
+        bishop_y += 1 if pair[0] == "1" else -1
+
+        bishop_x = max(bishop_x, 0)
+        bishop_y = max(bishop_y, 0)
+        bishop_x = min(bishop_x, GRAPH_WIDTH - 1)
+        bishop_y = min(bishop_y, GRAPH_HEIGHT - 1)
+
+        graph[bishop_y][bishop_x] += 1
+    
+    graph[GRAPH_HEIGHT // 2][GRAPH_WIDTH // 2] = 15 # start is always marked 15
+    graph[bishop_y][bishop_x] = 16 # same with end but 16
+
+    return graph
+
+bytes = bytes_to_pairs(encode_text("SHA256:s6N0OwlTDKjDez98kZRwUGZbTYaQUArv+EYC6sigFwA ben@eshwil"))
+data = run_algorithm(bytes)
+print_graph(GRAPH_WIDTH, GRAPH_HEIGHT, data, True, CHARACTERS)
