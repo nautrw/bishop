a76028865d7bc0f5ac5552a6f8a34797c747e08b
nautrw <nautrw@proton.me>
2026-08-27 12:12:41 -0400

    feat: add data option

--- src/bishopviz/cli.py
+++ src/bishopviz/cli.py
@@ -1,6 +1,9 @@
 import random
 import string
+import subprocess
+import sys
 import time
+from pathlib import Path
 
 import click
 
@@ -16,42 +19,54 @@ GRAPH_HEIGHT = 9
 @click.option('-a', '--animate', type=float, help="Display the algorithm's progress as an animation, with the specified number of seconds as the interval between frames.")
 @click.option('-c', '--charset', type=str, show_default=True, default="ascii", help="Use a different character set for the graph. Options: ascii, ascii_alt, emoji, emoji2, emoji3, emoji4, greek, cyrillic, katakana, math, blocks, faces, cars, plants")
 @click.option('-C', '--colors', is_flag=True, help="Whether to colorize the output.")
+@click.option('-d', '--data', is_flag=True, help="Will open a Streamlit app in the web browser to show statistics and graphs from the algorithm.")
 @click.option('--no-start-end', is_flag=True, help="Don't show the start and end positions on the graph.")
-def cli(random_feed: bool, animate: float, charset: str, colors: bool, no_start_end: bool,) -> None:
-    accomodate_large_chars = False
-
-    if charset not in CHARACTER_SETS:
-        raise click.ClickException("You must select one of the character set options. To view the available options, run `bishopviz --help`.")
-    elif charset in ["emoji", "emoji2", "emoji3", "emoji4", "katakana", "faces", "cars", "plants"]:
-        accomodate_large_chars = True
-
-    if random_feed:
-        feed = ''.join(random.choices(string.printable, k=32))
-
-    hash = text_md5(feed)
-    print(f'Hash: {hash.hexdigest()}')
-
-    byte_pairs = bytes_to_pairs(hash.digest())
-    algorithm = DrunkenBishopAlgorithm(GRAPH_WIDTH, GRAPH_HEIGHT, byte_pairs)
-
-    if animate:
-        for i in range(len(byte_pairs)):
-            algorithm.move()
-            print(f"Generation: {i + 1}")
-            algorithm.print_graph(CHARACTER_SETS[charset], show_start=not no_start_end, show_end=not no_start_end, show_current_position=True, accomodate_large_chars=accomodate_large_chars, colorize=colors)
-
-            if i != len(byte_pairs) - 1:
-                # basically this moves the cursor to the top of the box,
-                # giving the illusion of an animation
-                # \r is the carriage return, so it moves it to the beginning
-                # of the line
-                # \x1b[yA is an ANSI escape code, which moves the cursor `y`
-                # lines up
-                print(f'\r\x1b[{GRAPH_HEIGHT + 4}A')
-
-            time.sleep(animate)
+def cli(random_feed: bool, animate: float, charset: str, colors: bool, data: bool, no_start_end: bool,) -> None:
+    if data:
+        subprocess.Popen([
+            sys.executable,
+            "-m",
+            "streamlit",
+            "run",
+            str(Path(__file__).parent / "data.py"),
+            "--server.headless=true",
+            "--browser.gatherUsageStats=false"
+        ])
     else:
-        for _ in range(len(byte_pairs)):
-            algorithm.move()
+        accomodate_large_chars = False
 
-        algorithm.print_graph(CHARACTER_SETS[charset], show_start=not no_start_end, show_end=not no_start_end, show_current_position=False, accomodate_large_chars=accomodate_large_chars, colorize=colors)
+        if charset not in CHARACTER_SETS:
+            raise click.ClickException("You must select one of the character set options. To view the available options, run `bishopviz --help`.")
+        elif charset in ["emoji", "emoji2", "emoji3", "emoji4", "katakana", "faces", "cars", "plants"]:
+            accomodate_large_chars = True
+
+        if random_feed:
+            feed = ''.join(random.choices(string.printable, k=32))
+
+        hash = text_md5(feed)
+        print(f'Hash: {hash.hexdigest()}')
+
+        byte_pairs = bytes_to_pairs(hash.digest())
+        algorithm = DrunkenBishopAlgorithm(GRAPH_WIDTH, GRAPH_HEIGHT, byte_pairs)
+
+        if animate:
+            for i in range(len(byte_pairs)):
+                algorithm.move()
+                print(f"Generation: {i + 1}")
+                algorithm.print_graph(CHARACTER_SETS[charset], show_start=not no_start_end, show_end=not no_start_end, show_current_position=True, accomodate_large_chars=accomodate_large_chars, colorize=colors)
+
+                if i != len(byte_pairs) - 1:
+                    # basically this moves the cursor to the top of the box,
+                    # giving the illusion of an animation
+                    # \r is the carriage return, so it moves it to the beginning
+                    # of the line
+                    # \x1b[yA is an ANSI escape code, which moves the cursor `y`
+                    # lines up
+                    print(f'\r\x1b[{GRAPH_HEIGHT + 4}A')
+
+                time.sleep(animate)
+        else:
+            for _ in range(len(byte_pairs)):
+                algorithm.move()
+
+            algorithm.print_graph(CHARACTER_SETS[charset], show_start=not no_start_end, show_end=not no_start_end, show_current_position=False, accomodate_large_chars=accomodate_large_chars, colorize=colors)
