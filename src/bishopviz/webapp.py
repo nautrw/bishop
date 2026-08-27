a75e4435b0805034c44c3b9b9c9053d91ff89921
nautrw <nautrw@proton.me>
2026-08-27 12:59:35 -0400

    feat: add basic streamlit app

---
+++ src/bishopviz/webapp.py
@@ -0,0 +1,32 @@
+import random
+import string
+
+import plotly.express as px
+import streamlit as st
+
+from bishopviz.algorithm import DrunkenBishopAlgorithm
+from bishopviz.globals import GRAPH_HEIGHT, GRAPH_WIDTH
+from bishopviz.hash import bytes_to_pairs, text_md5
+
+st.header("Drunken Bishop Algorithm Data Visualization")
+
+feed = st.text_area(
+    "Feed",
+    # value=''.join(random.choices(string.printable, k=32)),
+)
+
+hash = text_md5(feed)
+st.write(f"Hash from text: {hash.hexdigest()}")
+
+byte_pairs = bytes_to_pairs(hash.digest())
+
+algorithm = DrunkenBishopAlgorithm(GRAPH_WIDTH, GRAPH_HEIGHT, byte_pairs)
+
+generation = st.slider("Generation", min_value=1, max_value=64, step=1, value=64, help="Change the slider to view the algorithm at a specific generation.")
+
+for _ in range(generation):
+    algorithm.move()
+
+fig = px.imshow(algorithm.graph, text_auto=True, color_continuous_scale="Viridis")
+
+st.plotly_chart(fig)
